025a5cc334575565111aacacbc79820ec0f8282e
Petar Petrov <angrifff93@gmail.com>
2022-07-05 15:21:54 +0300

    added exericse

---
+++ classes_and_objects/project7/car.py
@@ -0,0 +1,6 @@
+from project7.vehicle import Vehicle
+
+
+class Car(Vehicle):
+    def drive(self):
+        return 'driving...'
---
+++ classes_and_objects/project7/sports_car.py
@@ -0,0 +1,6 @@
+from project7.car import Car
+
+
+class SportsCar(Car):
+    def race(self):
+        return 'racing...'
