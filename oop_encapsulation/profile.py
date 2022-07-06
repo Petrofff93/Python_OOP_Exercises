199cc8f6af57afdb6067907d90c92258ac9809d3
Petar Petrov <angrifff93@gmail.com>
2022-07-06 10:51:16 +0300

    refactored encapsulation exercise

--- oop_encapsulation/profile.py
+++ oop_encapsulation/profile.py
@@ -1,16 +1,3 @@
-def correct_password(password):
-    is_upper = False
-    is_digit = False
-
-    for pa in password:
-        if pa.isupper():
-            is_upper = True
-        if pa.isdigit():
-            is_digit = True
-
-    return len(password) >= 8 and is_digit and is_upper
-
-
 class Profile:
     def __init__(self, username, password):
         self.username = username
@@ -21,28 +8,25 @@ class Profile:
         return self.__username
 
     @username.setter
-    def username(self, username):
-        if not 5 <= len(username) <= 15:
-            raise ValueError("The username must be between 5 and 15 characters.")
-        self.__username = username
-
-    def get_username(self):
-        return self.__username
+    def username(self, value):
+        if len(value) < 5 or len(value) > 15:
+            raise ValueError('The username must be between 5 and 15 characters.')
+        self.__username = value
 
     @property
     def password(self):
         return self.__password
 
     @password.setter
-    def password(self, password):
-        if not correct_password(password):
-            raise ValueError("The password must be 8 or more characters with at least 1 digit and 1 uppercase letter.")
-        self.__password = password
-
-    def get_password(self):
-        return self.__password
+    def password(self, value):
+        valid_length = len(value) >= 8
+        valid_upper_case = any(True for c in value if c.isupper())
+        valid_digit = any(True for c in value if c.isdigit())
+        if not (valid_length and valid_upper_case and valid_digit):
+            raise ValueError('The password must be 8 or more characters with at least 1 digit and 1 uppercase letter.')
+        self.__password = value
 
     def __str__(self):
-        return f'You have a profile with username: "{self.__username}" and password: {"*" * len(self.__password)}'
+        return f'You have a profile with username: "{self.username}" and password: {len(self.password)*"*"}'
 
 
