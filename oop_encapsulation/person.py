d32fd7cbd0b8f21c7aa01d2b2ce88a7adda609df
Petar Petrov <angrifff93@gmail.com>
2022-07-06 09:28:52 +0300

    added encapsulation exercise

---
+++ oop_encapsulation/person.py
@@ -0,0 +1,16 @@
+class Person:
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
+
+    def get_name(self):
+        return self.__name
+
+    def get_age(self):
+        return self.__age
+
+
+p = Person('Peter', 29)
+print(p.get_age())
+print(p.get_name())
+
