858b0dda5c7516b98df7f9a440f6f9e7afce48a2
Petar Petrov <angrifff93@gmail.com>
2022-07-13 12:06:10 +0300

    exercise refactor

--- oop_static_class_methods/shop.py
+++ oop_static_class_methods/shop.py
@@ -13,21 +13,17 @@ class Shop:
         if sum(self.items.values()) == self.capacity:
             return 'Not enough capacity in the shop'
 
-        if item_name in self.items:
-            self.items[item_name] += 1
-        else:
-            self.items[item_name] = 1
-
+        if item_name not in self.items:
+            self.items[item_name] = 0
+        self.items[item_name] += 1
         return f'{item_name} added to the shop'
 
     def remove_item(self, item_name, amount):
-        if item_name in self.items.keys():
-            if self.items[item_name] >= amount:
-                self.items[item_name] -= amount
-                if self.items[item_name] <= 0:
-                    del self.items[item_name]
-                return f'{amount} {item_name} removed from the shop'
-            return f'Cannot remove {amount} {item_name}'
+        if item_name in self.items.keys() and self.items[item_name] >= amount:
+            self.items[item_name] -= amount
+            if self.items[item_name] <= 0:
+                del self.items[item_name]
+            return f'{amount} {item_name} removed from the shop'
         return f'Cannot remove {amount} {item_name}'
 
     def __repr__(self):
